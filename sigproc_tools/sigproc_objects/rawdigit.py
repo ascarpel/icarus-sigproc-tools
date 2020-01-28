ce272148258a3b5829574fffe204700eb9f40e4c
Andrea Scarpelli <ascarpel@icarusgpvm04.fnal.gov>
2020-01-28 15:37:35 -0600

    Modified import scheme of waveforms in rawdigit.py for the new version of uproot

--- sigproc_tools/sigproc_objects/rawdigit.py
+++ sigproc_tools/sigproc_objects/rawdigit.py
@@ -5,9 +5,9 @@ import numpy as np
 
 class RawDigit:
     """
-    RawDigit: Emulates RawDigits as read into python via "uproot". Generally this means that we are given the 
-    "events" folder in the input root file which contains the RawDigits per event. We can then access each by 
-    event number 
+    RawDigit: Emulates RawDigits as read into python via "uproot". Generally this means that we are given the
+    "events" folder in the input root file which contains the RawDigits per event. We can then access each by
+    event number
     """
     def __init__(self,eventsFolder,producer):
         """
@@ -17,23 +17,23 @@ class RawDigit:
         self.eventsFolder = eventsFolder
         self.producer     = producer
         self.obj          = eventsFolder.array(self.producer+"obj",flatten=True)
-        
+
     def numEvents(self):
         numEvents = len(self.obj)
         return numEvents
-        
+
     def numChannels(self,eventNum):
         nChannels = self.obj[eventNum]
         return nChannels
-    
+
     def numTicks(self,eventNum,channelNum=0):
         samples = self.eventsFolder.array(self.producer+"obj.fSamples",entrystart=eventNum,entrystop=eventNum+1,flatten=True)
         return samples[channelNum]
-    
+
     def getWaveforms(self,eventNum):
         """
         Plan: Provided the RawDigits exists for a given event (e.g. in Multi-TPC readout an event may have no RawDigits),
-              we can look up the information to pull out the waveform from the data block. Interestingly, each waveform 
+              we can look up the information to pull out the waveform from the data block. Interestingly, each waveform
               will begin with a count (4096) and end with a guard (0) - except there is no count for the first and no guard
               for the last. So the below contortions are done to allow resizing and dropping of this extraneous info
         """
@@ -41,14 +41,12 @@ class RawDigit:
         if self.numChannels(eventNum) > 0:
             nTicks    = self.numTicks(eventNum)
             nChannels = self.numChannels(eventNum)
-            waveforms = self.eventsFolder.array(self.producer+"obj.fADC",entrystart=eventNum,entrystop=eventNum+1,flatten=True)
-            waveforms = np.concatenate([waveforms,[0,nTicks]])  # This adds the pattern "0, 4096" to the end of the 1D array
-            waveforms = waveforms.reshape(nChannels,nTicks+2)   # Now we make a 2D array of 384 channels by 4096+2 ticks
-            return waveforms[:,:-2]
+            buffer = self.eventsFolder.array(self.producer+"obj.fADC",entrystart=eventNum,entrystop=eventNum+1,flatten=True)
+            waveforms = np.array(buffer[0]).reshape(nChannels,nTicks)
+            return waveforms
         else:
             return numpy.zeros(shape=(1,1))
-        
+
     def getChannels(self,eventNum):
         channels = self.eventsFolder.array(self.producer+"obj.fChannel",entrystart=eventNum,entrystop=eventNum+1,flatten=True)
         return channels
-
